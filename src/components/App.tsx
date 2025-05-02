afdb8f440e7b03ce447bead3f90d5a6e66d7bc9a
Payton McIntosh <leynos@troubledskies.net>
2025-05-02 09:52:07 +0100

    Remove unneeded component

--- src/components/App.tsx
+++
@@ -1,9 +0,0 @@
-// src/App.tsx
-
-export default function App() {
-  return (
-    <div className="flex justify-center items-center bg-avocado-500 min-h-screen text-white">
-      <h1 className="font-display text-4xl">Craic</h1>
-    </div>
-  );
-}
